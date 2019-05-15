86888456ae83e62eb5abf2e09a3cebe58f792420
Alaa <alaulwan@hotmail.com>
2019-05-15 04:45:02 +0200

    Report and JARs files.

--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -6,7 +6,7 @@ import java.net.Socket;
 
 public class ServerMain {
 	public static final int BUFSIZE = 1024;
-	public final static int MYPORT = 4444;
+	public final static int MYPORT = 8888;
 
 	public static void main(String[] args) throws IOException {
 		int port = args.length >0 ? Integer.parseInt(args[0]):MYPORT;
@@ -14,7 +14,7 @@ public class ServerMain {
 		/* Create Server Socket */
 		@SuppressWarnings("resource")
 		ServerSocket serverSocket = new ServerSocket(port);
-		System.out.println("TCP Server is Running on the port: " + MYPORT + ", Buffer Size: " + BUFSIZE);
+		System.out.println("TCP Server is Running on the port: " + port + ", Buffer Size: " + BUFSIZE);
 		while (true)
 		 {
 			try {
